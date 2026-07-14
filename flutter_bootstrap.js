6bfaa1d626d2256884f5acb5b803f6ac48bb0e24
janaksiag1 <janaksiag1@gmail.com>
2026-07-14 19:01:21 +0530

    Deploy with correct photo favicons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1336325821"
+    serviceWorkerVersion: "2524924114"
   }
 });
